90b83aa276a7dd1cbc11c83fa041d741248943cc
sindhura <sindhura343434@gmail.com>
2023-08-26 22:05:19 +0530

    initial commit

--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -19,11 +19,81 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User register(String username, String password, String countryName) throws Exception{
+        if(!isValidCountryName(countryName)){
+            throw new Exception("Country not found");
+        }
 
+        //given to create new country for country name
+        Country country=new Country();
+        CountryName countryName1=getCountryName(countryName);
+        country.setCountryName(countryName1);
+        country.setCode(countryName1.toCode());
+
+        User user=new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setConnected(false);
+        user.setOriginalCountry(country);
+
+        country.setUser(user);
+        //need to save first to get id for user
+        userRepository3.save(user);
+
+        String originalIP=country.getCode()+"."+user.getId();
+        user.setOriginalIp(originalIP);
+
+        userRepository3.save(user);
+
+        return user;
     }
+
 
     @Override
     public User subscribe(Integer userId, Integer serviceProviderId) {
+        User user=userRepository3.findById(userId).get();
 
+        ServiceProvider serviceProvider=serviceProviderRepository3.findById(serviceProviderId).get();
+
+        user.getServiceProviderList().add(serviceProvider);
+        serviceProvider.getUsers().add(user);
+
+        serviceProviderRepository3.save(serviceProvider);
+
+        return user;
     }
+    private boolean isValidCountryName(String countryName){
+        //to be filled later
+        if (countryName.equalsIgnoreCase("IND")) {
+            return true;
+        } else if (countryName.equalsIgnoreCase("USA")) {
+            return true;
+        } else if (countryName.equalsIgnoreCase("AUS")) {
+            return true;
+        } else if (countryName.equalsIgnoreCase("CHI")) {
+            return true;
+        } else if (countryName.equalsIgnoreCase("JPN")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private CountryName getCountryName(String countryName) throws Exception {
+        if(countryName.equalsIgnoreCase("IND") ){
+            return CountryName.IND;
+        }
+        else if(countryName.equalsIgnoreCase("UDA") ){
+            return CountryName.USA;
+        }
+        else if(countryName.equalsIgnoreCase("AUS")){
+            return CountryName.AUS;
+        }
+        else if(countryName.equalsIgnoreCase("CHI")){
+            return CountryName.CHI;
+        }
+        else if (countryName.equalsIgnoreCase("JPN")){
+            return CountryName.JPN;
+        }
+        else{
+            throw new Exception("Country not found");
+        }
 }
